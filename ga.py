9f981b44cf153dda524d66420c329d90108a2e1b
deepti <deeptipgaonkar@gmail.com>
2015-03-18 00:54:36 +0530

    initialization done

--- ga.py
+++ ga.py
@@ -1,10 +1,12 @@
 import random
+import operator
 
 class FNRGeneticAlgorithm(object):
     def __init__(self, failed_sensor_nodes = [], nodes_info = {}, population_size = 100,  ):
         self.failed_sensor_nodes = failed_sensor_nodes
         self.sensor_nodes_info = nodes_info
         self.population_size = population_size
+        self.init_population = []
         self.current_population = []
     
     def __initialize(self):
@@ -12,7 +14,7 @@ class FNRGeneticAlgorithm(object):
             chromosome = []
             for j in range(len(self.failed_sensor_nodes)):
                 chromosome.append(random.randint(0,1))
-            self.self.current_population.append(chromosome)
+            self.self.init_population.append(chromosome)
     
     def __calculate_fitness_function(self, chromosome):
         """
@@ -22,7 +24,7 @@ class FNRGeneticAlgorithm(object):
         output = 2*1 -2*1 + 3*0+4*1-5*0+2*1-3*1+4*1+9*1+10*1 = 2-2+0+4-0+2-3+4+9+10=26
         """
         factors_list = [2,-2,3,4,-5,2,-3,4,9,10]
-        return {tuple(chromosome):sum([i*j for i,j in zip(chromosome, factors_list)])}
+        return tuple(chromosome),sum([i*j for i,j in zip(chromosome, factors_list)])
         
         
         pass
@@ -36,11 +38,15 @@ class FNRGeneticAlgorithm(object):
     def __mutation(self):
         pass
     
+    
     def get_solutions(self, iterations=2):
         self.__initialize()
-        for i in range(iterations):
-            
-                 
+        population = []
+        for chromosome in self.init_population:
+            key,value=self.__calculate_fitness_function(chromosome)
+            population.append((key,value))
+        population=sorted(population, key = lambda item: item[1], reverse = True)
+        
 def main():
     g = FNRGeneticAlgorithm(failed_sensor_nodes = [9, 7, 10, 81, 23, 57, 34, 46, 66, 70])
     g.get_solutions() # init population
